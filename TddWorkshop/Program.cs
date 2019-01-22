a46293f437705aba306d24992dde58d21081bd4f
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2019-01-22 13:20:24 -0500

    Ajoutez des fichiers projet.

---
+++ TddWorkshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TddWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
