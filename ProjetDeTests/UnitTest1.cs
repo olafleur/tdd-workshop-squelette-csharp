73cf5bd2bb15518719c27050640ee193da68b17b
Olivier Lafleur <olivier.lafleur@cll.qc.ca>
2019-01-22 13:37:18 -0500

    Ajout du projet de test

---
+++ ProjetDeTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TddWorkshop;
+
+namespace ProjetDeTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
